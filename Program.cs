d23804f50d0c65799bb36841ccd1bef315432860
Satyagopal UH6Z <uh6z@eurofins.com>
2022-05-30 12:02:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitGithubExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
